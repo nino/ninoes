27fab2f9dc388e1d79e42e8913b74f3802c93dbe
Nino Annighoefer <nino@ninoan.com>
2025-02-09 11:39:41 +0000

    Add supabase auth

---
+++ app/Auth.tsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import { supabase } from "./supabaseClient";
+
+export default function Auth() {
+  const [loading, setLoading] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    setLoading(true);
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if (error) {
+      alert(error.message);
+    } else {
+      alert("Logged in successfully!");
+    }
+    setLoading(false);
+  };
+
+  return (
+    <div className="row flex flex-center">
+      <div className="col-6 form-widget">
+        <h1 className="header">Supabase + React</h1>
+        <p className="description">
+          Sign in using your email and password below
+        </p>
+        <form className="form-widget" onSubmit={handleLogin}>
+          <div>
+            <input
+              className="inputField"
+              type="email"
+              placeholder="Your email"
+              value={email}
+              required={true}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div>
+            <input
+              className="inputField"
+              type="password"
+              placeholder="Your password"
+              value={password}
+              required={true}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div>
+            <button className="button block" disabled={loading}>
+              {loading ? <span>Loading</span> : <span>Sign in</span>}
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}

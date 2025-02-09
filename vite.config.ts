30a78427e16511b871178240b6578c31b359d2cf
Nino Annighoefer <nino@ninoan.com>
2025-02-09 15:11:41 +0000

    Configure Sentry Vite plugin for error tracking and source maps

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), sentryVitePlugin({
+    org: "none-9or",
+    project: "ninoes"
+  })],
+
+  build: {
+    sourcemap: true
+  }
 });
